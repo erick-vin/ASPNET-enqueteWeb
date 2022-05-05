6ba3593d2bb1614022a309451b5628dfb40c70ed
erick.santos3 <erick.santos@segfy.com>
2022-05-05 17:58:54 -0300

    Criando aplicação web para realização de enquetes

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using EnqueteWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnqueteWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Responder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Responder(Resposta resposta)
+        {
+            Repositorio.AdicionarRespota(resposta);
+            return View("Obrigado");
+        }
+
+        public IActionResult Resultado()
+        {
+            return View(Repositorio.Respostas);
+        }
+
+    }
+}
---
+++ Models/Repositorio.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace EnqueteWeb.Models
+{
+    public class Repositorio
+    {
+        private static List<Resposta> respostas = new List<Resposta>();
+        public static IEnumerable<Resposta> Respostas { get{ return respostas; } }
+        public static void AdicionarRespota(Resposta resposta)
+        {
+            respostas.Add(resposta);
+        }
+
+        static Repositorio()
+        {
+            respostas.Add(new Resposta()
+                {Nome = "Fulano", Email = "fulano@gmail.com", Sim = true});
+            respostas.Add(new Resposta()
+                {Nome = "Cicrano", Email = "cicrano@gmail.com", Sim = true});
+            respostas.Add(new Resposta()
+                {Nome = "Beltrano", Email = "beltrano@gmail.com", Sim = true});
+            respostas.Add(new Resposta()
+                {Nome = "Maria", Email = "maria@gmail.com", Sim = true});
+            respostas.Add(new Resposta()
+                {Nome = "José", Email = "jose@gmail.com", Sim = true});
+            respostas.Add(new Resposta()
+                {Nome = "Fulano", Email = "fulano@gmail.com", Sim = true});
+        }
+    }
+}
